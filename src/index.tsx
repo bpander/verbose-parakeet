1935aaa0d700dc6c3b8c847f8c98c96d7477d5cf
Brad Anderson <>
2019-10-19 18:40:06 -0400

    Wires up redux dev tools cuz why not.

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
 import App from './components/App';
 import { rootReducer } from './redux-modules';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
-)
+);
