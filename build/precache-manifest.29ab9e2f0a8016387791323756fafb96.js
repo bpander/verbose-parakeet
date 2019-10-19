afc50aae5eb8df2ed57c19e84a9296cae66b5e00
Brad Anderson <>
2019-10-19 18:52:17 -0400

    Quick and dirty deploy

---
+++ build/precache-manifest.29ab9e2f0a8016387791323756fafb96.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d3ce2b65f27fc989e764f7f99f5ed9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b647706bb010a760b16c",
+    "url": "/static/js/2.110f84ff.chunk.js"
+  },
+  {
+    "revision": "11ec8e41ddddefbb064a",
+    "url": "/static/js/main.46c0ad8e.chunk.js"
+  },
+  {
+    "revision": "ef4c03a973f6ea5659eb",
+    "url": "/static/js/runtime-main.2dff9f32.js"
+  }
+]);
