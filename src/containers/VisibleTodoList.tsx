4f92c71531d8f56e354a0afdfcd8327d0deab3ea
Brad Anderson <>
2019-10-19 17:03:29 -0400

    Moves selector to selectors directory

--- src/containers/VisibleTodoList.tsx
+++ src/containers/VisibleTodoList.tsx
@@ -1,26 +1,12 @@
 import { connect } from 'react-redux'
 import { toggleTodo } from '../actions'
 import TodoList from '../components/TodoList'
-import { VisibilityFilter } from '../enums/VisibilityFilter'
-import { TodosState } from 'reducers/todos'
 import { RootState } from 'reducers/RootState'
 import { Dispatch } from 'redux'
-
-const getVisibleTodos = (todos: TodosState, filter: VisibilityFilter) => {
-  switch (filter) {
-    case VisibilityFilter.All:
-      return todos
-    case VisibilityFilter.Completed:
-      return todos.filter(t => t.completed)
-    case VisibilityFilter.Active:
-      return todos.filter(t => !t.completed)
-    default:
-      throw new Error('Unknown filter: ' + filter)
-  }
-}
+import { getVisibleTodos } from 'selectors'
 
 const mapStateToProps = (state: RootState) => ({
-  todos: getVisibleTodos(state.todos, state.visibilityFilter)
+  todos: getVisibleTodos(state.visibilityFilter)(state.todos),
 });
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
